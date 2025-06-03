490ff4560799d364694ff63a6ecdcfdda19c7bcc
Mykhailo Stratan <36572099+MykhailoStratan@users.noreply.github.com>
2025-06-03 16:18:15 -0700

    Add theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { InputText } from 'primereact/inputtext';
 
 // import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
 import 'primereact/resources/primereact.min.css'; //core css
-import "primereact/resources/themes/lara-light-cyan/theme.css";
 import 'primeicons/primeicons.css'; //icons
 // import 'primeflex/primeflex.css'; // flex
 import './App.css';
--- src/components/MenuBar/MenuBar.tsx
+++ src/components/MenuBar/MenuBar.tsx
@@ -3,10 +3,36 @@
 import { Menubar } from 'primereact/menubar';
 import { MenuItem } from 'primereact/menuitem';
 import { Image } from 'primereact/image';
+import { useState, useEffect } from 'react';
 import '../MenuBar/MenuBar.css';
 
 
 export default function MenuBar() {
+    const [theme, setTheme] = useState<'light' | 'dark'>('light');
+
+    useEffect(() => {
+        const stored = localStorage.getItem('theme');
+        const link = document.getElementById('theme-link') as HTMLLinkElement | null;
+        if (stored === 'dark' && link) {
+            link.href = '/themes/lara-dark-indigo/theme.css';
+            setTheme('dark');
+        }
+    }, []);
+
+    const switchTheme = () => {
+        const link = document.getElementById('theme-link') as HTMLLinkElement | null;
+        if (!link) return;
+        if (theme === 'light') {
+            link.href = '/themes/lara-dark-indigo/theme.css';
+            localStorage.setItem('theme', 'dark');
+            setTheme('dark');
+        } else {
+            link.href = '/themes/lara-light-cyan/theme.css';
+            localStorage.setItem('theme', 'light');
+            setTheme('light');
+        }
+    };
+
     const items: MenuItem[] = [
         {
             label: 'Home',
@@ -30,29 +56,36 @@ export default function MenuBar() {
         }
     ];
 
+    const themeIcon = theme === 'light' ? 'pi pi-sun' : 'pi pi-moon';
+
     const end = (
-        <ul className='social-media-wrapper'>
-            <li className="social-media-item">
-                <a href="https://www.instagram.com/d.stoikova.photo?igsh=MWwwdnBndHd1b2w4ZA==" target='_blank'>
-                    <img src="/instagram_icon.png" alt="instagram" className="social-media-icon" />
-                </a>
-            </li>
-            <li className="social-media-item">
-                <a href="https://www.facebook.com/profile.php?id=100011219850763" target='_blank'>
-                    <img src="/facebook_icon.png" alt="facebook" className="social-media-icon" />
-                </a>
-            </li>
-            <li className="social-media-item">
-                <a href="https://t.me/Daryna_Stoikova" target='_blank'>
-                    <img src="/telegram_icon.png" alt="telegram" className="social-media-icon" />
-                </a>
-            </li>
-            <li className="social-media-item">
-                <a href="">
-                    <img src="/linkedin_icon.png" alt="linkedin" className="social-media-icon" />
-                </a>
-            </li>
-        </ul>
+        <div className="end-wrapper">
+            <ul className='social-media-wrapper'>
+                <li className="social-media-item">
+                    <a href="https://www.instagram.com/d.stoikova.photo?igsh=MWwwdnBndHd1b2w4ZA==" target='_blank'>
+                        <img src="/instagram_icon.png" alt="instagram" className="social-media-icon" />
+                    </a>
+                </li>
+                <li className="social-media-item">
+                    <a href="https://www.facebook.com/profile.php?id=100011219850763" target='_blank'>
+                        <img src="/facebook_icon.png" alt="facebook" className="social-media-icon" />
+                    </a>
+                </li>
+                <li className="social-media-item">
+                    <a href="https://t.me/Daryna_Stoikova" target='_blank'>
+                        <img src="/telegram_icon.png" alt="telegram" className="social-media-icon" />
+                    </a>
+                </li>
+                <li className="social-media-item">
+                    <a href="">
+                        <img src="/linkedin_icon.png" alt="linkedin" className="social-media-icon" />
+                    </a>
+                </li>
+            </ul>
+            <button className="theme-toggle p-link" onClick={switchTheme} aria-label="Switch theme">
+                <i className={themeIcon}></i>
+            </button>
+        </div>
     );
 
     const start = (
@@ -74,4 +107,3 @@ export default function MenuBar() {
         </div>
     )
 }
-        
